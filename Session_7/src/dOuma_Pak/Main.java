690e3367b256ca2942fa2f6fd4b7d2c8d0af9dea
Crazydrag <raunakkochrekar001@gmail.com>
2021-07-04 16:09:25 +0530

    Edited1

--- Session_7/src/dOuma_Pak/Main.java
+++ Session_7/src/dOuma_Pak/Main.java
@@ -5,7 +5,7 @@ public class Main {
 	public static void main(String[] args) {
 		
         Book bk = new Book();
-        Book bk1 = new Book("Raunak","Freedom", 845);
+        Book bk1 = new Book("Raunak","Solo leveling", 999);
         bk.setData();
         bk.display();
         bk1.display();
